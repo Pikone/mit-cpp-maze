90f93d8baf91ce7b478b5fffcc06bd62261de946
Pikksel One <47038939+Pikone@users.noreply.github.com>
2020-07-28 15:19:17 +0300

    A lot of changes

--- lab.cpp
+++ lab.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #define WALL '*'
 #define NO_WALL ' '
+#define PATH '#'
 //#define MAP_NUM 10  //Define Ипользовался в ранних версиях программы
 
 using namespace std;
@@ -14,6 +15,7 @@ int map_bg [100][100];		//Фоновой массив следящий за пе
 int i = 0, z = 0, map_size = 0;
 int x = 1, y = 1;	//position
 int map_num = 0;	//Временная переменная, чтобы проверить ее на не четность и передать значение в map_size
+int for_count_bg_map = 3;	//Переменная для записи ее в фоновой массив
 
 void map_print()	//Вывод массива на экран
 {
@@ -25,6 +27,16 @@ void map_print()	//Вывод массива на экран
 	}
 }
 
+void map_bg_print()
+{
+	for (i = 0; i < map_size; i++)
+	{
+		for (z = 0; z < map_size; z++)
+			cout << map_bg[i][z] << "\t";
+		cout << endl;
+	}
+}
+
 void map_bg_fill()	//Заполнение background массива
 {
 	for (i = 0; i < map_size; i++)
@@ -32,6 +44,15 @@ void map_bg_fill()	//Заполнение background массива
 		for (z = 0; z < map_size; z++)
 			map_bg[i][z] = 0;
 	}
+
+	for (i = 0; i < map_size; i++)
+	{
+		map_bg[0][i] = 1;
+		map_bg[i][0] = 1;
+		map_bg[map_size - 1][i] = 1;
+		map_bg[i][map_size - 1] = 1;
+	}
+	map_bg[1][1] = 2; 	//Тактический костыль
 }
 
 void map_draw_grid()	//Отрисовка сетки (без вывода)
@@ -53,13 +74,61 @@ void map_draw_grid()	//Отрисовка сетки (без вывода)
 	}
 }
 
-void map_draw_lab()		//Алгоритм создания лабиринта
+void map_draw_lab()		//Алгоритм создания лабиринта (один шаг)
 {
 
 	switch(rand() % 4)
 	{
 		case 0:		//up
-			if (map_bg [x -= 2][y] < 1) // возьми алгоритм со змейки dude
+			if (map_bg [x - 1][y] != 1) // возьми алгоритм со змейки dude
+			{
+				if (map_bg [x - 1][y] < 1)
+				{
+					map_bg[--x][y] = for_count_bg_map; for_count_bg_map++; cout << "x" << " " << x << " ";
+					map[x][y] = PATH; cout << x << " ";
+					map_bg[--x][y] = for_count_bg_map; for_count_bg_map++; cout << x << " ";
+					map[x][y] = PATH; cout << x << " " << endl;
+				}
+			}
+		break;
+
+		case 1:		//right
+			if (map_bg [x][y + 1] != 1)
+			{
+				if (map_bg [x][y + 1] < 1)
+				{
+					map_bg[x][++y] = for_count_bg_map; for_count_bg_map++; cout << "y" << " " << y << " ";
+					map[x][y] = PATH; cout << y << " ";
+					map_bg[x][++y] = for_count_bg_map; for_count_bg_map++; cout << y << " ";
+					map[x][y] = PATH; cout << y << " " << endl;
+				}
+			}
+		break;
+
+		case 2:		//down
+			if (map_bg [x + 1][y] != 1)
+			{
+				if (map_bg [x + 1][y] < 1)
+				{
+					map_bg[++x][y] = for_count_bg_map; for_count_bg_map++; cout << "x" << " " << x << " ";
+					map[x][y] = PATH; cout << x << " ";
+					map_bg[++x][y] = for_count_bg_map; for_count_bg_map++; cout << x << " ";
+					map[x][y] = PATH; cout << x << " " << endl;
+				}
+			}
+		break;
+
+		case 3:		//left
+			if (map_bg [x][y - 1] != 1)
+			{
+				if (map_bg [x][y - 1] < 1)
+				{
+					map_bg[x][--y] = for_count_bg_map; for_count_bg_map++; cout << "y" << " " << y << " ";
+					map[x][y] = PATH; cout << y << " ";
+					map_bg[x][--y] = for_count_bg_map; for_count_bg_map++; cout << y << " ";
+					map[x][y] = PATH; cout << y << " " << endl;
+				}
+			}
 		break;
 	}
 }
@@ -75,9 +144,14 @@ int main ()
 		map_size = map_num;
 
 	map_bg_fill();		//Заполнение массива backgroung
-	map_draw_grid();
+	map_draw_grid();	//Отрисовка сетки (без вывода)
+	map_print();		//Вывод массива на экран
+	map_bg_print();
+	do{
+	map_draw_lab();		//Вызов алгоритма отрисовывания шага лабиринта
+	}while(map_bg[x - 1][y] == 0 || map_bg[x + 1][y] == 0 || map_bg[x][y - 1] == 0 || map_bg[x][y + 1] == 0);
 	map_print();
-	map_draw_lab();
+	map_bg_print();
 
 return 0;
 }
